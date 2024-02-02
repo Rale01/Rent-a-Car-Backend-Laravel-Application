7dc7c7d255f4894736ce94658d5e5d21f4b04deb
Rastko Jokic <rale01jokic@gmail.com>
2024-02-02 18:12:32 +0100

    Added routes for authetification

--- rent-a-car-backend/routes/api.php
+++ rent-a-car-backend/routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//login i registracija
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
